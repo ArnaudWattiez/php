0e16f57a37a811a85f0cfaf6a9e8bc09372e3f22
arnaud.wattiez.pro@gmail.com <arnaud.wattiez.pro@gmail.com>
2023-07-05 11:29:37 +0200

    initial commit

---
+++ plusgrandpluspetit.php
@@ -0,0 +1,18 @@
+<?php
+// Traduction en PHP de l'algo + grand + petit
+$nbcoup = 1;
+$nbgenere = rand(1, 100);
+// var_dump($nbgenere);
+$nbsaisie = readline("choisis un nombre entre 1 et 100: ");
+while ($nbsaisie != $nbgenere) {
+    $nbcoup++;
+    if ($nbsaisie > $nbgenere) {
+        $nbsaisie = intval(readline("veuillez entrer nombre plus petit: "));
+        // $nbcoup++;
+    } else if ($nbsaisie < $nbgenere) {
+        $nbsaisie = intval(readline("veuillez entrer un nombre plus grand: "));
+        // $nbcoup++;
+    }
+};
+
+echo ("succès! en " . $nbcoup . " coups");
